9dea666235908d55ca845538ba45b588f374921c
Adarsh kumar <adarshdbg428@gmail.com>
2020-12-08 17:12:52 +0530

    first

---
+++ Array/number-of-students-doing-homework-at-a-given-time.py
@@ -0,0 +1,9 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        ans=0
+        for i in range(len(startTime)):
+            if startTime[i]<=queryTime and endTime[i]>=queryTime:
+                ans+=1
+                
+        return ans
+        
